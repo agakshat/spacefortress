bd81bbd5511c340173f77676b518cdea3694b0f6
Ryan Hope <rmh3093@gmail.com>
2017-07-07 16:29:32 -0400

    change package from ssf to spacefortress (ssf exists)

---
+++ python/spacefortress/README.rst
@@ -0,0 +1,1 @@
+A python wrapper to c-spacefortress
--- python/spacefortress/setup.py
+++ python/spacefortress/setup.py
@@ -17,14 +17,25 @@ deps.update({
 sources = [os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../src/%s" % f)) for f in ["ssf.c","ssf_cairo.c"]]
 headers = [os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../src/%s" % f)) for f in ["ssf.h","ssf_cairo.h"]]
 
-extension_mod = Extension("ssf/libssfcairo", sources, **deps)
+extension_mod = Extension("spacefortress/libssfcairo", sources, **deps)
+
+descr_file = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(
-    name = "ssf",
+    name = "spacefortress",
     version = '%s' % GVERSION,
+    description='A python wrapper to c-spacefortress',
+    long_description=open(descr_file).read(),
     author='Shawn Betts',
     author_email='sabetts@andrew.cmu.edu',
     url='https://bitbucket.org/andersonlab/c-spacefortress',
-    packages=['ssf'],
-    ext_modules=[extension_mod]
+    packages=['spacefortress'],
+    ext_modules=[extension_mod],
+    license='GPL-2',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
