45389faac15aaf55c8816e77e032ffd469c87696
Ryan Hope <rmh3093@gmail.com>
2017-07-12 19:16:33 -0400

    add tasks for feature and image based envs

--- python/spacefortress.gym/spacefortress/gym/__init__.py
+++ python/spacefortress.gym/spacefortress/gym/__init__.py
@@ -3,35 +3,63 @@ from gym.envs.registration import register
 from .ssf_env import SSF_Env
 
 register(
-   	id='SpaceFortress-explode-v0',
-   	entry_point='spacefortress.gym:SSF_Env',
-    kwargs={ 'gametype': 'explode' },
-    nondeterministic=False,
+	id='SpaceFortress-explode-image-v0',
+	entry_point='spacefortress.gym:SSF_Env',
+	kwargs={ 'gametype': 'explode', 'obs_type': 'image' },
+	nondeterministic=False
 )
 
 register(
-   	id='SpaceFortress-autoturn-v0',
-   	entry_point='spacefortress.gym:SSF_Env',
-    kwargs={ 'gametype': 'autoturn' },
-    nondeterministic=False,
+	id='SpaceFortress-autoturn-image-v0',
+	entry_point='spacefortress.gym:SSF_Env',
+	kwargs={ 'gametype': 'autoturn', 'obs_type': 'image' },
+	nondeterministic=False
+)
+
+register(
+	id='SpaceFortress-explode-features-v0',
+	entry_point='spacefortress.gym:SSF_Env',
+	kwargs={ 'gametype': 'explode', 'obs_type': 'features' },
+	nondeterministic=False
+)
+
+register(
+	id='SpaceFortress-autoturn-features-v0',
+	entry_point='spacefortress.gym:SSF_Env',
+	kwargs={ 'gametype': 'autoturn', 'obs_type': 'features' },
+	nondeterministic=False
 )
 
 add_group(
-    id= 'SpaceFortress',
-    name= 'SpaceFortress',
-    description= '32 levels of the original Super Mario Bros game.'
+	id= 'SpaceFortress',
+	name= 'SpaceFortress',
+	description= 'SpaceFortress games'
 )
 
 add_task(
-   	id='SpaceFortress-explode-v0',
-   	summary="2D frictionless space shooter (explode variant)",
-   	group='SpaceFortress',
-   	contributor='Ryan M. Hope',
+	id='SpaceFortress-explode-image-v0',
+	summary="2D frictionless space shooter (explode, image)",
+	group='SpaceFortress',
+	contributor='Ryan M. Hope'
 )
 
 add_task(
-   	id='SpaceFortress-autoturn-v0',
-   	summary="2D frictionless space shooter (autoturn variant)",
-   	group='SpaceFortress',
-   	contributor='Ryan M. Hope',
+	id='SpaceFortress-autoturn-image-v0',
+	summary="2D frictionless space shooter (autoturn, image)",
+	group='SpaceFortress',
+	contributor='Ryan M. Hope'
+)
+
+add_task(
+	id='SpaceFortress-explode-features-v0',
+	summary="2D frictionless space shooter (explode, features)",
+	group='SpaceFortress',
+	contributor='Ryan M. Hope'
+)
+
+add_task(
+	id='SpaceFortress-autoturn-features-v0',
+	summary="2D frictionless space shooter (autoturn, features)",
+	group='SpaceFortress',
+	contributor='Ryan M. Hope'
 )
