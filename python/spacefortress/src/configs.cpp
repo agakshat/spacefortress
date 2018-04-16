56cd679815edd87de8f6f8fb7375fe5c4ef62fad
agakshat <agarwalaks30@gmail.com>
2018-04-16 15:48:24 -0400

    correction in angle shaping

--- python/spacefortress/src/configs.cpp
+++ python/spacefortress/src/configs.cpp
@@ -25,7 +25,7 @@ void baseConfig(Config *c) {
   c->set("staircase", false);
   c->set("fortressPointsPerContraction", 10);
 
-  c->set("turningReward", 0);
+  c->set("turningReward", 0.0);
   // Fortress
   c->set("fortressSectorSize", 10);
   c->set("fortressLockTime", 1000);
@@ -90,7 +90,7 @@ Config *testexplodeConfig() {
   c->set("destroyFortress", 100);
   c->set("gameTime", 180000);
   c->set("maxPoints", 3748);
-  c->set("turningReward", 1);
+  c->set("turningReward", 1.0);
   return c;
 }
 
@@ -109,7 +109,7 @@ Config *explodeConfig() {
   baseConfig(c);
   c->set("gameTime", 180000);
   c->set("maxPoints", 3748);
-  c->set("turningReward", 1);
+  c->set("turningReward", 0.1);
   return c;
 }
 
@@ -120,7 +120,7 @@ Config *nopenaltyexplodeConfig() {
   c->set("maxPoints", 3748);
   c->set("missilePenalty", 0);
   c->set("missPenalty", 0);
-  c->set("turningReward", 1);
+  c->set("turningReward", 0.1);
   return c;
 }
 
@@ -143,6 +143,6 @@ Config *slowexplodeConfig() {
   c->set("missilePenalty",2);
   c->set("missPenalty",5);
   c->set("shellSpeed",2);
-  c->set("turningReward", 1);
+  c->set("turningReward", 0.1);
   return c;
 } 
